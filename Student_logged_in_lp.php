79956a23935251c1d4b831345726c252e4f9d2ea
Nate <scott.pearce3@gmail.com>
2018-11-07 18:49:35 +0000

    Inital Upload of php/html files

---
+++ Student_logged_in_lp.php
@@ -0,0 +1,41 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+   
+    <link href="style_sheets/Student_page_style.css" rel="stylesheet"
+          type="text/css" /> 
+	<title><?php echo $_POST["StudentID"] ; ?>'s information</title>
+</head>
+<body>
+    <div class="topnav">
+        <div class="flex-container">
+            <div style="flex-grow:1">
+                <h2>Brakebills College for Magical Pedagogy</h2>
+                <?php echo $_POST["StudentID"]; ?><br />
+            </div>
+            <div style="flex-grow:2">
+                <ul>
+                    <li><a href="Student_logged_in_lp.html">Home</a></li>
+                    <li><a href="Student_logged_in_lp.html">My Modules</a></li>
+                    <li><a href="Student_logged_in_lp.html">Module Library</a></li>
+                    <li><a href="Student_logged_in_lp.html">Edit Enrolment</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <button type="button" onclick="alert('Your Modules will Go Here')">What are my Modules</button>
+        <button type="button" onclick="alert('Module Selection will go Here')">Change your Modules</button><br />
+    </div>
+    
+    <div class="container">
+        <button type="button" onclick="alert('Modules Offered will go here ')">Module Catalouge</button>
+        <button type="button" onclick="alert('Other Stuff Will go here')">Other Stuff</button>
+		
+
+    </div>
+</body>
+</html>
---
+++ php_secrets.php
@@ -0,0 +1,25 @@
+﻿<?php
+$name = psxnp4_uni_data
+$password = h!l!ghtMEplz2
+$server_name = mysql.cs.nott.ac.uk
+
+
+function passwordcheck($database,$givenpword,$userID)
+    $password_comp = "SELECT passwrd FROM loginin 
+	                  INNER JOIN students ON students.studentID = login.studentID
+					  where studentID = '" .$userID. "'"
+	$password_line = $db->query($password_comp);
+	$intermidiate = $password_line->fetch_assoc();
+	$comparitor = $intermidiate['passwrd']
+	if ($givenpword = $comparitor) {
+	    /* send them to the log in page */
+		header("Location: http://mersey.cs.nott.ac.uk/~psxnp4/Student_logged_in_lp.php"); 
+		exit();
+}	else 
+		/* Redirect browser back to the login page */
+		header("Location: http://mersey.cs.nott.ac.uk/~psxnp4/Breakbills_LogIn.php"); 
+		exit();
+
+
+?>
+
